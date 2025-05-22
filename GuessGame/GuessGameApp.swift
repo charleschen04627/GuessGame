9ffd55de6167020239dbb5899d02ee8f618bcb65
CharlesChen <coge04627@gmail.com>
2025-05-22 13:26:51 +0800

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by Charles on 22/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
